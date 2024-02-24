0eda7b1d46983961e0a9f432f438e028817a5321
ZA-MA <zama.mz.2004@gmail.com>
2024-02-24 20:02:37 +0500

    viewModels

---
+++ worco-backend/Models/ViewModels/ViewElements.cs
@@ -0,0 +1,20 @@
+﻿namespace worco_backend.Models.ViewModels
+{
+    public class ViewAddElement
+    {
+        public string image { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public string type { get; set; }
+        public bool only_indicator { get; set; }
+        public int indicator_x { get; set; }
+        public int indicator_y { get; set; }
+        public int indicator_size { get; set; }
+    }
+
+    public class ViewDeleteElement
+    {
+        public int id { get; set; }
+    }
+
+}
---
+++ worco-backend/Models/ViewModels/ViewPlace.cs
@@ -0,0 +1,15 @@
+﻿namespace worco_backend.Models.ViewModels
+{
+    public class ViewSavePositionPlaces
+    {
+        public int id { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+    public class ViewListPlaces
+    {
+        public List<Place> ListPlaces { get; set;}
+    }
+
+}
