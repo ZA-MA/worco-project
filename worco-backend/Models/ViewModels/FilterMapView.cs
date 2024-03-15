06efd1ae3f269dc58c5c953b1ddd2d2db436cbf0
ZA-MA <zama.mz.2004@gmail.com>
2024-03-15 21:21:04 +0500

    еще файлы

---
+++ worco-backend/Models/ViewModels/FilterMapView.cs
@@ -0,0 +1,17 @@
+﻿namespace worco_backend.Models.ViewModels
+{
+    public class FilterMapView
+    {
+        public SelectedTypes? selectedTypes {  get; set; }
+        public List<DateOnly>? SelectedDates { get; set; }
+        public List<string>? selectedOptions { get; set; }
+    }
+
+    public class SelectedTypes
+    {
+        public bool place { get; set; }
+        public bool meetingRoom { get; set; }
+        public bool office { get; set; }
+    }
+}
+
